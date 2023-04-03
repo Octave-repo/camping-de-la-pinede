56794d274e638718d9961f8c89be48610f24098b
Octave-portfolio <valadieroctave@gmail.com>
2023-04-03 09:05:58 +0200

    20 - Affichage de la liste des campings

---
+++ web/src/views/CampingSearchView.vue
@@ -0,0 +1,95 @@
+<template>
+    <h1>Chercher un camping</h1>
+    <div class="filter">
+        <table>
+            <tr>
+                <th>Ville</th>
+                <th>Equipement</th>
+                <th>Type de logement</th>
+                <th>Prix Max</th>
+                <th>Note</th>
+                <th>Nombre d'étoiles</th>
+            </tr>
+            <tr>
+                <th><input type="text"></th>
+                <th><select></select></th>
+                <th><select></select></th>
+                <th><input type="number" style="width: 25%;"></th>
+                <th><input type="number" style="width: 25%;"></th>
+                <th><input type="number" style="width: 25%;"></th>
+            </tr>
+        </table>
+    </div>
+    <br>
+    <div class="campingDisplay">
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Etoiles</th>
+                <th>Note</th>
+                <th>Adresse</th>
+            </tr>
+            <tr v-for="camping in listCampings" :key="camping.id" class="campingObject" @click="goCamping(camping.id)">
+                <td>{{ camping.nom }}</td>
+                <td>{{ camping.prix }}</td>
+                <td>{{ camping.nombreEtoiles }} </td>
+                <td>{{ camping.note }}</td>
+                <td>{{ camping.adresse.nom }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+import Map from '@/components/Map.vue';
+import CampingService from '@/service/CampingService'
+export default {
+  name: 'CampingSearchView',
+  components: {
+    Map
+  },
+  data(){
+    return {
+        listCampings: []
+
+    }
+  },
+  beforeMount(){
+    this.getCampingList();
+  },
+  methods: {
+    async getCampingList(){
+        try{
+            let response = await CampingService.getCampings();
+            this.listCampings = response.data;
+        }
+        catch(error){
+            console.log("RIEN");
+        }
+    },
+    goCamping(id){
+        this.$router.push({name: 'campingView', params: {id}});
+    }
+  }
+}
+</script>
+<style scoped>
+.campingObject:hover{
+    background-color: lightgray;
+}
+table {
+    border-collapse: collapse;
+    }
+    tr { 
+    border: solid;
+    border-width: 1px 0;
+    }
+    tr:first-child {
+     border-top: none;
+    }
+    tr:last-child {
+        border-bottom: none;
+    }
+
+</style>
