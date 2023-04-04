1715bf8e0c27eb0e050d3816f48c618776053565
MrADOY <pietrzak.aurelien@gmail.com>
2023-04-04 11:27:11 +0200

    Fix camping post

--- Backend/camping/src/main/java/fr/camping/entity/GPSLocation.java
+++ Backend/camping/src/main/java/fr/camping/entity/GPSLocation.java
@@ -19,7 +19,7 @@ public
 class GPSLocation{
     @Id
     @GeneratedValue
-    private String id;
+    private Long id;
     private String nom;
     private double longitude;
     private double latitude;
