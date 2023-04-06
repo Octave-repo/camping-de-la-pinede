1114b3e0b13c54de03014dee67058a4578182949
Octave-portfolio <valadieroctave@gmail.com>
2023-04-06 23:30:38 +0200

    Changement de campings vers camping (ce qui causait l'erreur CORS)

--- web/src/api/CampingApi.js
+++ web/src/api/CampingApi.js
@@ -5,16 +5,19 @@ const url = 'http://localhost:'
 const mock_port = '8081'
 
 const path = `${url}${mock_port}`
+const headers = {
+    headers:{}
+}
 class CampingApi{
     //TODO Changer pour faire fonctionner avec les filtres
     getCampings(){
-        return InstanceAxios.get(`${path}/campings`);
+        return InstanceAxios.get(`${path}/camping/`);
     }
     getCamping(id){
-        return InstanceAxios.get(`${path}/campings/${id}`);
+        return InstanceAxios.get(`${path}/camping/${id}`);
     }
     postCamping(content){
-        return InstanceAxios.post(`${path}/campings/`, content);
+        return InstanceAxios.post(`${path}/camping/`, content);
     }
 }
 
